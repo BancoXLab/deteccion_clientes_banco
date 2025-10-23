fb807936acf291d07a49fffeaad61b676b517956
MrHousesm <facucasas7@gmail.com>
2025-10-23 13:43:47 +0000

    commit packages scr

---
+++ scr/ingesta/__init__.py
@@ -0,0 +1,5 @@
+# Package marker for scr.ingesta
+from .Ingesta_de_datos import remove_duplicates, load_data, ingesta, transformar, etl_banco, esritura
+from .Perfil_de_datos import profile_data
+from .esquema_DB import definir_esquema
+from .encoding import enc_preprocessor
